d9f0293b9f80f2089debfb12f006da2b9f0bd38d
Andrew Cumming <andrewdcumming@hotmail.com>
2021-05-03 13:20:05 +0100

    Added Forms. Next Add directory loading to find images then display image list on the program

---
+++ ImageAnalysisTool/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace ImageAnalysisTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.newButton = new System.Windows.Forms.Button();
+            this.loadButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // newButton
+            // 
+            this.newButton.Location = new System.Drawing.Point(164, 124);
+            this.newButton.Name = "newButton";
+            this.newButton.Size = new System.Drawing.Size(155, 142);
+            this.newButton.TabIndex = 0;
+            this.newButton.Text = "New";
+            this.newButton.UseVisualStyleBackColor = true;
+            this.newButton.Click += new System.EventHandler(this.newButton_Click);
+            // 
+            // loadButton
+            // 
+            this.loadButton.Location = new System.Drawing.Point(436, 124);
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Size = new System.Drawing.Size(156, 142);
+            this.loadButton.TabIndex = 1;
+            this.loadButton.Text = "Load";
+            this.loadButton.UseVisualStyleBackColor = true;
+            this.loadButton.Click += new System.EventHandler(this.loadButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.loadButton);
+            this.Controls.Add(this.newButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button newButton;
+        private System.Windows.Forms.Button loadButton;
+    }
+}
+
